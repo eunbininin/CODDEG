e2ea9cb2189fc5adca40b08909b2a3360684ffc4
jinseok kim <121952875+jinseok19@users.noreply.github.com>
2024-11-28 04:14:19 +0900

    flask con 50%

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from erp_combination import erp_combination
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/step1')
+def step1():
+    return render_template('step1.html')
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
+@app.route('/run_erp_combination', methods=['POST'])
+def run_erp_combination():
+    # Call the erp_combination function with appropriate arguments
+    erp_combination(
+        screen_width=1920,
+        screen_height=1080,
+        fs=256,
+        channels=['EEG_Fp1', 'EEG_Fp2'],
+        isi=1000,
+        top_image_path='./images/tops/T1.jpg',
+        clothes_type='bottoms',
+        image_folder='./images/bottoms',
+        num_trials=10,
+        num_images=30,
+        event_save_path='./event',
+        result_dir='./plot/bottoms/0',
+        lowcut=1.0,
+        highcut=30.0,
+        tmin=-0.2,
+        tmax=1.0,
+        mode='all'
+    )
+    return redirect(url_for('step1'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
