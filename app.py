d0b5caeb9c0b717e5acdb9dbe2aaccef9ea1c859
jinseok kim <121952875+jinseok19@users.noreply.github.com>
2024-11-28 04:43:25 +0900

    flask con 70%

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def report():
     return render_template('report.html')
 
 @app.route('/run_erp_combination', methods=['POST'])
-def run_erp_combination():
+def run_erp_combination_TOP():
     # Call the erp_combination function with appropriate arguments
     erp_combination(
         screen_width=1920,
@@ -25,10 +25,10 @@ def run_erp_combination():
         fs=256,
         channels=['EEG_Fp1', 'EEG_Fp2'],
         isi=1000,
-        top_image_path='./images/tops/T1.jpg',
+        top_image_path='./images/backgrounds/B0.jpg',
         clothes_type='bottoms',
         image_folder='./images/bottoms',
-        num_trials=10,
+        num_trials=1,
         num_images=30,
         event_save_path='./event',
         result_dir='./plot/bottoms/0',
@@ -40,5 +40,28 @@ def run_erp_combination():
     )
     return redirect(url_for('step1'))
 
+@app.route('/run_erp_combination_bt', methods=['POST'])
+def run_erp_combination_BOTTOM():
+    # Call the erp_combination function with appropriate arguments
+    erp_combination(
+        screen_width=1920,
+        screen_height=1080,
+        fs=256,
+        channels=['EEG_Fp1', 'EEG_Fp2'],
+        isi=1000,
+        top_image_path='./images/backgrounds/B0.jpg',
+        clothes_type='bottoms',
+        image_folder='./images/tops',
+        num_trials=1,
+        num_images=30,
+        event_save_path='./event',
+        result_dir='./plot/tops/0',
+        lowcut=1.0,
+        highcut=30.0,
+        tmin=-0.2,
+        tmax=1.0,
+        mode='all'
+    )
+    return redirect(url_for('step1'))
 if __name__ == '__main__':
     app.run(debug=True)
